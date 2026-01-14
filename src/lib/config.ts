01c8df9b874b37f5d393f4d278b9da7d128eff8e
Renato Navarro A. <72818352+devrenatonavarro@users.noreply.github.com>
2026-01-14 18:31:56 -0500

    agregando cors

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   // API_URL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-  API_URL: import.meta.env.VITE_API_URL || 'https://facturacion-api.lobitoconsulting.store/',
+  API_URL: import.meta.env.VITE_API_URL || 'https://facturacion-api.lobitoconsulting.store',
 }
